13212997cd2a11bea2acd69fd97628e1af674cfb
Sergei <Corvusincvr@gmail.com>
2025-06-16 08:59:59 +0300

    Completing quest by mini game

--- Assets/Scripts/Game/MiniGames/KitchenMiniGame.cs
+++ Assets/Scripts/Game/MiniGames/KitchenMiniGame.cs
@@ -1,12 +1,18 @@
+using System;
+using Game.Quests;
 using UnityEngine;
 
 namespace Game.MiniGames
 {
     public class KitchenMiniGame : IMiniGame
     {
+        public QuestType QType { get; } = QuestType.Kitchen;
+        public event Action<QuestType> OnMiniGameComplete;
+
         public void StartGame()
         {
             Debug.Log("Kitchen Mini Game Started");
+            OnMiniGameComplete?.Invoke(QType);
         }
     }
 }
