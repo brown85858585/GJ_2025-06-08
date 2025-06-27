7f937f8b080bb94cc298ec3bd8be946d74847b7a
Sergei <Corvusincvr@gmail.com>
2025-06-27 03:54:00 +0300

    Add PopupInteractionView to manage interaction icons and update prefab properties

---
+++ Assets/Scripts/UI/PopupInteractionView.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using Game;
+using Game.Interactions;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class PopupInteractionView : MonoBehaviour
+    {
+        [SerializeField] private Image popupImage;
+        [SerializeField] private List<Sprite> sprites;
+
+        public void SetInteractionImage(ItemCategory interactable)
+        {
+            
+            Sprite chosenSprite;
+            switch (interactable)
+            {
+                case ItemCategory.Bed:
+                    chosenSprite = sprites[6];
+                    break;
+                case ItemCategory.Flower:
+                    chosenSprite = sprites[4];
+                    break;
+                case ItemCategory.Kitchen:
+                    chosenSprite = sprites[1];
+                    break;
+                case ItemCategory.WateringCan:
+                    chosenSprite = sprites[3];
+                    break;
+                case ItemCategory.Computer:
+                    chosenSprite = sprites[2];
+                    break;
+                case ItemCategory.Books:
+                    chosenSprite = sprites[0];
+                    break;
+                case ItemCategory.None:
+                case ItemCategory.Door:
+                case ItemCategory.Guitar:
+                case ItemCategory.Painting:
+                case ItemCategory.Basket:
+                case ItemCategory.Window:
+                default:
+                    chosenSprite = sprites[5];
+                    break;
+            }
+            
+            popupImage.sprite = chosenSprite;
+
+            // Вариант 1. Устанавливает размер точно под исходные пиксели спрайта:
+            popupImage.SetNativeSize();
+        }
+    }
+}
